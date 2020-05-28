2e65d964e0f18e868bbbfd1b82b98fe4fc60fe69
Andrew Roberts <clownfragment@gmail.com>
2020-05-28 15:26:06 -0600

    for loops

---
+++ counting.py
@@ -0,0 +1,9 @@
+binary = ['dog', 'bear', 'cat']
+
+for i in binary:
+    for j in binary:
+        for k in binary:
+            for l in binary:
+                for m in binary:
+                    print(f'{i}, {j}, {k}, {l}, {m}')
+
