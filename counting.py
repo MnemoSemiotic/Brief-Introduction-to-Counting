f94588a9867fa6f79d52cb2dd4676a5c2dd50d65
Andrew Roberts <clownfragment@gmail.com>
2020-05-28 15:43:31 -0600

    abstract func

--- counting.py
+++ counting.py
@@ -1,9 +1,27 @@
-binary = ['dog', 'bear', 'cat']
+def dec_to_base_x(dec, base, n_places=4):
+    lst = []
+    x = dec
 
-for i in binary:
-    for j in binary:
-        for k in binary:
-            for l in binary:
-                for m in binary:
-                    print(f'{i}, {j}, {k}, {l}, {m}')
+    for _ in range(n_places):
+        place = x % base
+        lst.append(place)
+        x //= base
 
+    return lst[::-1] 
+
+
+def get_binary_in_range(n_places=8):
+    counts_d = {}
+
+    for i in range(0, 2**n_places):
+        counts_d[i] = dec_to_binary(i, n_places)
+    
+    return counts_d
+
+
+
+if __name__ == "__main__":
+    print(dec_to_base_x(76, 7, n_places=10))
+
+    # for k, v in get_binary_in_range(n_places=8).items():
+    #     print(f'{k}: {v}')
